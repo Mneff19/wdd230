38af176790f73f1109d6dbbbf797250ddfe91358
mneff19 <mpneff19@gmail.com>
2023-05-04 19:36:38 -0600

    added dark theme toggle, finished hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
+const themeToggle = document.querySelector("#theme-toggle");
 const mobileToggle = document.querySelector("#mobile-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("t-dark");
+    themeToggle.querySelectorAll(".icon").forEach((icon) => {
+        icon.classList.toggle("active");
+    });
+});
 
 mobileToggle.addEventListener("click", () => {
     mobileToggle.querySelectorAll(".icon").forEach((icon) => {
