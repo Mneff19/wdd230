6e71127793a85643b140b2970f5bd8fc2d25c1dc
mneff19 <mpneff19@gmail.com>
2023-07-11 13:23:20 -0600

    completed team activity

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,35 @@
 const themeToggle = document.querySelector("#theme-toggle");
 const mobileToggle = document.querySelector("#mobile-toggle");
 const visitsCounter = document.querySelector("#visit_counter");
+const weatherText = document.querySelector('#weatherText');
+const weatherIcon = document.querySelector('#weatherIcon');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=48.85792197297915&lon=2.353898423790552&appid=84fd1805bbdfc8774858391be52c31e4&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function displayResults(data) {
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherText.innerHTML = `${data.main.temp}&deg;F, ${desc}`;
+}
+
+apiFetch();
 
 themeToggle.addEventListener("click", () => {
     document.querySelector("body").classList.toggle("t-dark");
