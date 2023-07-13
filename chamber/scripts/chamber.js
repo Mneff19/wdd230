8d01a624336ecdbf5aa325bac413fb16da80b2e4
mneff19 <mpneff19@gmail.com>
2023-07-13 14:56:53 -0600

    added dynamic spotlight generation to homepage

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -2,6 +2,7 @@ const mobileToggle = document.querySelector("#mobile-toggle");
 const weatherTemp = document.querySelector("#weatherTemp");
 const weatherDescription = document.querySelector("#weatherDescription");
 const forecast = document.querySelector("#forecast");
+const spotlightHolder = document.querySelector("#spotlightHolder");
 
 const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=40.53183096915482&lon=-112.30080357417363&appid=84fd1805bbdfc8774858391be52c31e4&units=imperial';
 
@@ -59,6 +60,51 @@ function displayResults(data) {
 }
 
 // apiFetch();
+
+async function getJSONData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    populateSpotlight(data);
+}
+const members = getJSONData("https://mneff19.github.io/wdd230/chamber/data/members.json");
+
+function populateSpotlight(data) {
+    const ul = document.createElement("ul");
+    ul.classList.add("spotlight-list");
+
+    let paidMembers = []
+    for (let member in data) {
+        console.log(data[member].name + " membership_level is " + data[member].membership_level);
+        if (data[member].membership_level != "basic") {
+            paidMembers.push(member);
+        }
+    }
+
+    let spotlightOne = paidMembers[Math.floor(Math.random() * paidMembers.length)];
+    let spotlightTwo = paidMembers[Math.floor(Math.random() * paidMembers.length)];
+
+    while (spotlightOne == spotlightTwo) {
+        spotlightTwo = paidMembers[Math.floor(Math.random() * paidMembers.length)];
+    }
+
+    let spotlights = [spotlightOne, spotlightTwo];
+
+    for (let spotlight in spotlights) {
+        const li = document.createElement("li");
+        li.classList.add("spotlight-item");
+        li.innerHTML = `
+        <h3 class="body">${data[spotlights[spotlight]].adCopy}</h3>
+        <div class="bottom">
+            <p class="source">${data[spotlights[spotlight]].name}</p>
+            <a href="${data[spotlights[spotlight]].website}"><img src="${data[spotlights[spotlight]].img}" class="logo" alt="Logo of ${data[spotlights[spotlight]].name}, a local company" height="37.6"></a>
+        </div>
+        `;
+
+        ul.appendChild(li);
+    }
+
+    spotlightHolder.appendChild(ul);
+}
 
 mobileToggle.addEventListener("click", () => {
     mobileToggle.querySelectorAll(".icon").forEach((icon) => {
